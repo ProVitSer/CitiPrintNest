4dc7641be689481b22df84fe77370f50e405eaed
ProVitSer <prokinvs@gmail.com>
2022-01-25 20:26:23 +0000

    Синхронизация телефонной книги с 1с

--- Bitrix/src/app.module.ts
+++ Bitrix/src/app.module.ts
@@ -5,9 +5,10 @@ import { LoggerModule } from './logger/logger.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/config.provides';
 import { MongoModule } from './mongo/mongo.module';
+import { SyncDataModule } from './sync-data/sync-data.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] }), LoggerModule, MongoModule],
+  imports: [ConfigModule.forRoot({ load: [configuration] }), LoggerModule, MongoModule, SyncDataModule],
   controllers: [AppController],
   providers: [AppService],
 })
