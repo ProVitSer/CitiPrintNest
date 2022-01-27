89b451b046d6dba2911ae509cfc2674f2821acc1
ProVitSer <prokinvs@gmail.com>
2022-01-27 19:52:06 +0000

    test взаимодействие с Битрикс по вх\исх вызовам

--- Bitrix/src/asterisk/asterisk-ami.service.ts
+++ Bitrix/src/asterisk/asterisk-ami.service.ts
@@ -1,7 +1,14 @@
+import { BitrixApiService } from '@app/bitrix/bitrix.api.service';
 import { LoggerService } from '@app/logger/logger.service';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AsteriskHungupEvent, CallTypeContext } from './types/interfaces';
+import { AsteriskHangupIncomingEventAppData, AsteriskHangupOutgoingEventAppData, AsteriskHangupEvent, CallTypeContext, getBitrixStatusByAsterisk } from './types/interfaces';
+import { UtilsService } from '@app/utils/utils.service'
+import { PostgresService } from '@app/postgres/postgres.service';
+import { BitrixCallStatusType, BitrixCallType, CallFinishData, CallRegisterData, GetTaskResponse } from '@app/bitrix/types/interfaces';
+import { CollectionType, DbRequestType } from '@app/mongo/types/types';
+import { BitrixUsers, Tasks } from '@app/mongo/schemas';
+import { MongoService } from '@app/mongo/mongo.service';
 
 @Injectable()
 export class AmiService {
@@ -12,6 +19,9 @@ export class AmiService {
         @Inject('AMI') private readonly ami: any,
         private readonly configService: ConfigService,
         private readonly log: LoggerService,
+        private readonly bitrix: BitrixApiService,
+        private readonly pg: PostgresService,
+        private readonly mongo: MongoService
         
     ) {
     }
@@ -25,21 +35,24 @@ export class AmiService {
             this.client.on('namiConnectionClose', () => this.connectionClose());
             this.client.on('namiLoginIncorrect', () => this.loginIncorrect());
             this.client.on('namiInvalidPeer', () => this.invalidPeer());
-            this.client.on('namiEventNewexten', (event: AsteriskHungupEvent) => this.parseAmiEvent(event));
+            this.client.on('namiEventNewexten', (event: AsteriskHangupEvent) => this.parseAmiEvent(event));
         } catch (e) {
             this.log.error(`AMI onApplicationBootstrap ${e}`)
         }
 
     };
 
-    private async parseAmiEvent(event: AsteriskHungupEvent): Promise<void>{
+    private async parseAmiEvent(event: AsteriskHangupEvent): Promise<void> {
         if(event.context == CallTypeContext.Incoming && event.application == 'NoOp'){
             this.log.info(`Завершился входящий вызов на Asterisk ${event.appdata}`);
-            const phoneEvent = JSON.parse(event.appdata);
+            const phoneEvent: AsteriskHangupIncomingEventAppData = JSON.parse(event.appdata);
+            this.sendInfoByIncomingCall(phoneEvent);
         }
         else if(event.context == CallTypeContext.Outgoing && event.application == 'NoOp'){
             this.log.info(`Завершился исходящие вызов на Asterisk ${event.appdata}`);
-            const phoneEvent = JSON.parse(event.appdata);
+            const phoneEvent: AsteriskHangupOutgoingEventAppData = JSON.parse(event.appdata);
+            this.sendInfoByOutgoingCall(phoneEvent);
+
         }
     }
 
@@ -60,5 +73,170 @@ export class AmiService {
         this.log.error(`Invalid AMI Salute. Not an AMI?`);
         process.exit();
         
+    }
+
+    private async sendInfoByOutgoingCall(appData: AsteriskHangupOutgoingEventAppData): Promise<void>{
+        try {
+            const { exten /*исходящий номер*/ , unicueid, extensionNumber/*добавочный абонента*/, billsec, disposition, recording, start, end } = appData;
+            const { bitrixId } = await this.getBitrixUserID(extensionNumber);
+            const callStartData: CallRegisterData = {
+                bitrixId: bitrixId, 
+                phoneNumber: exten,
+                type: BitrixCallType.outgoing, 
+                callTime: start
+            }
+            const { CALL_ID } = await this.bitrix.externalCallRegister(callStartData);
+            
+            const callFinishData: CallFinishData = {
+                callId: CALL_ID, 
+                bitrixId: bitrixId, 
+                bilsec: Number(billsec), 
+                callStatus: getBitrixStatusByAsterisk[disposition], 
+                callType: BitrixCallType.outgoing,
+                recording: recording
+            }
+    
+            await this.bitrix.externalCallFinish(callFinishData);
+            return await this.checkCompletionTask(exten);
+        }catch(e){
+            this.log.error(`sendInfoByOutgoingCall ${e}`)
+        }
+    }
+
+    private async checkCompletionTask(incomingNumber: string){
+        try {
+            const result = await this.checkTaskExist(incomingNumber);
+            if (result != null) {
+                await this.bitrix.closeTask(result._id);
+                const params = {
+                    criteria: {
+                        _id: incomingNumber
+                    },
+                    entity: CollectionType.tasks,
+                    requestType: DbRequestType.delete,
+                }
+                return await this.mongo.mongoRequest(params);
+            }
+        }catch(e){
+            this.log.error(`checkCompletionTask ${e}`)
+        }
+    }
+
+    private async sendInfoByIncomingCall(appData: AsteriskHangupIncomingEventAppData): Promise<void>{
+        try {
+            const { unicueid, incomingNumber, billsec, disposition, recording, start, end } = appData;
+            const result = await this.pg.getExternalCallInfo(incomingNumber);
+            const bitrixCallStatusType = (result.isAnswered == true) ? BitrixCallStatusType.SuccessfulCall : BitrixCallStatusType.MissedCall;
+            const { bitrixId } = await this.getBitrixUserID(result.lastCallUser);
+            const bitrixUserId = (bitrixId == undefined)? this.configService.get("bitrix.custom.adminId") : bitrixId;
+            const callStartData: CallRegisterData = {
+                bitrixId: bitrixId, 
+                phoneNumber: incomingNumber,
+                type: BitrixCallType.incoming, 
+                callTime: start
+            }
+            const { CALL_ID } = await this.bitrix.externalCallRegister(callStartData);
+            
+            const callFinishData: CallFinishData = {
+                callId: CALL_ID, 
+                bitrixId: bitrixId, 
+                bilsec: Number(billsec), 
+                callStatus: bitrixCallStatusType, 
+                callType: BitrixCallType.incoming,
+                recording: recording
+            }
+
+            await this.bitrix.externalCallFinish(callFinishData);
+            if(this.configService.get("bitrix.custom.createTask") == true && bitrixCallStatusType == BitrixCallStatusType.MissedCall){
+                await this.createOrUpdateTask(bitrixId, result.lastCallUser, incomingNumber);
+            }
+        }catch(e){
+            this.log.error(`sendInfoByIncomingCall ${e}`)
+        }
+    }
+
+    private async createOrUpdateTask(bitrixId: string, extension: string, incomingNumber: string): Promise<any>{
+        try {
+            const result = await this.checkTaskExist(incomingNumber);
+            if(result == null){
+                const resultCreateTask = await this.bitrix.createTask({bitrixId: Number(bitrixId), incomingNumber: incomingNumber });
+                const params = {
+                    criteria: {},
+                    entity: CollectionType.bitrixUsers,
+                    requestType: DbRequestType.insertMany,
+                    data:  {
+                        _id: incomingNumber,
+                        taskId: resultCreateTask.result.task.id,
+                        bitrixUserId: bitrixId,
+                        extension: extension,
+                    }
+                }
+                return await this.mongo.mongoRequest(params)
+            } else {
+                return await this.updateTask(result._id, bitrixId, incomingNumber, extension);
+            }
+        }catch(e){
+            this.log.error(`checkIfTaskExist ${e}`)
+        }
+    }
+
+    private async updateTask(taskId: string, bitrixId: string, incomingNumber: string, extension: string): Promise<any>{
+        try {
+            const status = await this.bitrix.getTaskStatus(taskId);
+            if(status != false){
+                //Проверка один и тот же пользователь не ответил по данному вызову 
+                const task = status.result.task;
+                if(task.status == this.configService.get("bitrix.custom.taskIdStart") && task.responsibleId == bitrixId){
+                    return;
+                } else { //Другой пользователь не ответил на вызов, меняем ответственного
+                    await this.bitrix.updateResponsibleIdTask(taskId, bitrixId);
+                    const params = {
+                        criteria: {
+                            _id: incomingNumber
+                        },
+                        entity: CollectionType.tasks,
+                        requestType: DbRequestType.updateById,
+                        data:  {
+                            $set: { bitrixUserId: bitrixId, 
+                                    extension: extension 
+                                }
+                        }
+                    };
+                    return await this.mongo.mongoRequest(params);
+                }
+            }
+        }catch(e){
+            this.log.error(`updateTask ${e}`)
+        }
+    }
+
+    private async checkTaskExist(incomingNumber: string): Promise<Tasks>{
+        try {
+            const params = {
+                criteria: {
+                    _id: incomingNumber
+                },
+                entity: CollectionType.tasks,
+                requestType: DbRequestType.findAll,
+            }
+            return await this.mongo.mongoRequest(params);
+        } catch(e){
+            this.log.error(`checkTaskExist ${e}`)
+        }
+    }
+
+    private async getBitrixUserID(userExtension: string): Promise<BitrixUsers>{
+        try {
+            const params = {
+                criteria: {
+                    extension: userExtension
+                },
+                entity: CollectionType.bitrixUsers,
+                requestType: DbRequestType.findAll,
+            }
+            return await this.mongo.mongoRequest(params);
+        } catch(e){
+            this.log.error(`getBitrixUserID ${e}`)
+        }
     }
 }
